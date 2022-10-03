b303357d2a54de9609a87a4ba85bd963ab6c47fb
alain-kubwayo <ahkubwayo@gmail.com>
2022-10-03 15:23:34 +0200

    add more content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/*.html'],
+  content: ['./public/*.html', './public/pages/*.html'],
   theme: {
     extend: {
       fontFamily: {
